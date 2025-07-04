fd23bb74007c79789a654f3035fa181bf5b07f8c
shiba0627 <1E8EC3@gmail.com>
2025-07-04 09:51:06 +0900

    v1.3

--- config.py
+++ config.py
@@ -1,17 +1,18 @@
 YES = './img/YES.png'
 YES_WAIT = './img/YES_wait.png'
-YES_CLICKED = './img/YES_clicked.png'
+YES_SELECTED = './img/YES_selected.png'
 NO = './img/NO.png'
 NO_WAIT = './img/NO_wait.png'
-NO_CLICKED = './img/NO_clicked.png'
+NO_SELECTED = './img/NO_selected.png'
 
-CANDIDATES_1 = './img/moriya_default.png'
-CANDIDATES_1_WAIT = './img/moriya_wait.png'
-CANDIDATES_1_CLICKED = './img/moriya_clicked.png' 
+CANDIDATES_1 = './img/hunahashi.png'
+CANDIDATES_1_WAIT = './img/hunahashi_wait.png'
+CANDIDATES_1_SELECTED = './img/hunahashi_selected.png'
+CANDIDATES_1_WAIT_SELECTED = './img/hunahashi_wait_select.png'
 
 #変数
 BUTTON_SIZE = 175
 CANDIDATES_WHIDTH = 320//2.5#320
-CANDIDATES_HEIGHT = 1000//2.5#1000
+CANDIDATES_HEIGHT = 1200//2.5#1000
 HOVER_TIME = 1#滞留時間s
 ARC_RADIUS = 30
